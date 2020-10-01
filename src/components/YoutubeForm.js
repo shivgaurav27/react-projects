83f48b9bd2fba69772f11ecc33fa9260410210aa
shivgaurav7727 <shivgaurav27@gmail.com>
2020-10-01 16:03:31 +0530

    use onBlur and touched  to check which filed user visted and show error on visited field only

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -47,30 +47,33 @@ const YoutubeForm = () => {
                     <label htmlFor='name'>Name</label>
                     <input type='text' id='text' name='name'
                         onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         value={formik.values.name}
                     />
                     {
-                        formik.errors.name && <div className='error'>{formik.errors.name}</div>
+                        formik.touched.name && formik.errors.name && <div className='error'>{formik.errors.name}</div>
                     }
                 </div>
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
                     <input type='email' id='email' name='email'
                         onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         value={formik.values.email}
                     />
                     {
-                        formik.errors.email && <div className='error'>{formik.errors.email}</div>
+                        formik.touched.email && formik.errors.email && <div className='error'>{formik.errors.email}</div>
                     }
                 </div>
                 <div className='form-control'>
                     <label htmlFor='name'>Channel</label>
                     <input type='text' id='Channel' name='Channel'
                         onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         value={formik.values.Channel}
                     />
                     {
-                        formik.errors.Channel && <div className='error'>{formik.errors.Channel}</div>
+                        formik.touched.Channel && formik.errors.Channel && <div className='error'>{formik.errors.Channel}</div>
                     }
                 </div>
                 <button type='submit'>Submit</button>

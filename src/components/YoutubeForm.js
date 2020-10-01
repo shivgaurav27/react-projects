f5eccda83deafa94a2242d733a97887571a3942e
shivgaurav7727 <shivgaurav27@gmail.com>
2020-10-01 16:21:37 +0530

    we can use getFieldProps in formik to reduce repeated code and fast coding

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -52,9 +52,7 @@ const YoutubeForm = () => {
                 <div className='form-control'>
                     <label htmlFor='name'>Name</label>
                     <input type='text' id='text' name='name'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.name}
+                    {...formik.getFieldProps('name')}
                     />
                     {
                         formik.touched.name && formik.errors.name && <div className='error'>{formik.errors.name}</div>
@@ -63,9 +61,8 @@ const YoutubeForm = () => {
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
                     <input type='email' id='email' name='email'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.email}
+                    {...formik.getFieldProps('email')}
+
                     />
                     {
                         formik.touched.email && formik.errors.email && <div className='error'>{formik.errors.email}</div>
@@ -74,9 +71,8 @@ const YoutubeForm = () => {
                 <div className='form-control'>
                     <label htmlFor='name'>Channel</label>
                     <input type='text' id='Channel' name='Channel'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.Channel}
+                    {...formik.getFieldProps('Channel')}
+
                     />
                     {
                         formik.touched.Channel && formik.errors.Channel && <div className='error'>{formik.errors.Channel}</div>

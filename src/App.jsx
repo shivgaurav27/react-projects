9f72578bfd6daa11620e8496ad29e6cf3828935b
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-18 11:03:41 +0530

    material ui

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles();
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              color="textSecondary"
+              paragraph
+              align="center"
+            >
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
+              voluptate ab architecto commodi, quas repellendus consequuntur
+              itaque corrupti provident assumenda nostrum! Sit, excepturi facere
+              commodi debitis aut officiis perspiciatis soluta!
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Seondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;

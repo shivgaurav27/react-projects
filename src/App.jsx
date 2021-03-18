974e87990852ec2d1d38e47a33b254e1726a1041
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-18 11:44:20 +0530

    completed photo gallery using material ui

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,21 @@ import {
   Button,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles();
-
+import useStyles from "./styles";
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const App = () => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -49,7 +48,7 @@ const App = () => {
               itaque corrupti provident assumenda nostrum! Sit, excepturi facere
               commodi debitis aut officiis perspiciatis soluta!
             </Typography>
-            <div>
+            <div className={classes.buttons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary">
@@ -65,7 +64,47 @@ const App = () => {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/1600x900/?beach"
+                    title="image Title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card . you can use this section to
+                      describe the content
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View{" "}
+                    </Button>
+                    <Button size="small" color="primary">
+                      edit{" "}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Something here for footer
+        </Typography>
+      </footer>
     </>
   );
 };

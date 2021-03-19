f2aae7588548ac42bc73066a83f27d53ab38b0c6
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-19 15:24:03 +0530

    re usable radio group btn added

---
+++ src/components/controls/RadioGroup.js
@@ -0,0 +1,27 @@
+import {
+  FormControl,
+  RadioGroup as MiuiRadioGroup,
+  FormLabel,
+  Radio,
+  FormControlLabel,
+} from "@material-ui/core";
+import React from "react";
+
+export default function RadioGroup(props) {
+  console.log("props", props);
+  const { name, label, value, onChange, items } = props;
+  return (
+    <FormControl>
+      <FormLabel>{label}</FormLabel>
+      <MiuiRadioGroup row name={name} value={value} onChange={onChange}>
+        {items.map((item) => (
+          <FormControlLabel
+            value={item.id}
+            control={<Radio />}
+            label={item.title}
+          />
+        ))}
+      </MiuiRadioGroup>
+    </FormControl>
+  );
+}
--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -3,13 +3,18 @@ import {
   FormControlLabel,
   FormLabel,
   Grid,
-  makeStyles,
   Radio,
   RadioGroup,
 } from "@material-ui/core";
-import React, { useState, useEffect } from "react";
-import Input from "../../components/controls/Input";
+import React from "react";
+import Controls from "../../components/controls/Controls";
 import Form, { UseForm } from "../../components/UseForm";
+
+const genderItems = [
+  { id: "male", title: "male" },
+  { id: "female", title: "Female" },
+  { id: "other", title: "other" },
+];
 
 const initialValues = {
   id: 0,
@@ -22,6 +27,7 @@ const initialValues = {
   hireDate: new Date(),
   iaPermanent: false,
 };
+
 export default function EmployeeForm() {
   const { values, setValues, handleInputChange } = UseForm(initialValues);
 
@@ -30,14 +36,14 @@ export default function EmployeeForm() {
       <Form>
         <Grid container>
           <Grid item xs={6}>
-            <Input
+            <Controls.Input
               name="fullName"
               label="Full Name"
               value={values.fullName}
               onChange={handleInputChange}
             />
 
-            <Input
+            <Controls.Input
               variant="outlined"
               label="Email"
               name="email"
@@ -46,32 +52,13 @@ export default function EmployeeForm() {
             />
           </Grid>
           <Grid item xs={6}>
-            <FormControl>
-              <FormLabel>
-                <RadioGroup
-                  row
-                  name="gender"
-                  value={values.gender}
-                  onChange={handleInputChange}
-                >
-                  <FormControlLabel
-                    value="male"
-                    control={<Radio />}
-                    label="Male"
-                  />
-                  <FormControlLabel
-                    value="female"
-                    control={<Radio />}
-                    label="Female"
-                  />
-                  <FormControlLabel
-                    value="other"
-                    control={<Radio />}
-                    label="Other"
-                  />
-                </RadioGroup>
-              </FormLabel>
-            </FormControl>
+            <Controls.RadioGroup
+              name="gender"
+              label="Gender"
+              value={values.gender}
+              onChange={handleInputChange}
+              items={genderItems}
+            />
           </Grid>
         </Grid>
       </Form>

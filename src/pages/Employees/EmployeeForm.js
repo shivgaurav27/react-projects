f7fae4071ca37ff210e57a56bfc10ad4a3693900
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-19 13:24:39 +0530

    re use textfields

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -1,5 +1,15 @@
-import { Grid, makeStyles, TextField } from "@material-ui/core";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  makeStyles,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@material-ui/core";
 import React, { useState, useEffect } from "react";
+import Input from "../../components/controls/Input";
 import Form, { UseForm } from "../../components/UseForm";
 
 const initialValues = {
@@ -21,13 +31,13 @@ export default function EmployeeForm() {
       <Form>
         <Grid container>
           <Grid item xs={6}>
-            <TextField
-              variant="outlined"
+            <Input
+              name="fullName"
               label="Full Name"
               value={values.fullName}
-              name="fullName"
               onChange={handleInputChange}
-            ></TextField>
+            />
+
             <TextField
               variant="outlined"
               label="Email"
@@ -36,7 +46,34 @@ export default function EmployeeForm() {
               onChange={handleInputChange}
             ></TextField>
           </Grid>
-          <Grid item xs={6}></Grid>
+          <Grid item xs={6}>
+            <FormControl>
+              <FormLabel>
+                <RadioGroup
+                  row
+                  name="gender"
+                  value={values.gender}
+                  onChange={handleInputChange}
+                >
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Male"
+                  />
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Female"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Other"
+                  />
+                </RadioGroup>
+              </FormLabel>
+            </FormControl>
+          </Grid>
         </Grid>
       </Form>
     </>

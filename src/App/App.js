dd14c82faa173df420edd23bde74c4eb9b2b51d3
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-19 11:17:11 +0530

    custome theme added

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,9 @@
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import {
+  createMuiTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
 import React from "react";
 import Header from "../components/Header";
 import SidebMenu from "../components/SidebMenu";
@@ -11,16 +16,32 @@ const useStyles = makeStyles({
   },
 });
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#7986cb",
+      main: "#3f51b5",
+    },
+    secondary: {
+      light: "#ff4081",
+      main: "#f50057",
+    },
+    background: {
+      default: "#f4f5fd",
+    },
+  },
+});
+
 function App() {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SidebMenu />
       <div className={classes.appMain}>
         <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 

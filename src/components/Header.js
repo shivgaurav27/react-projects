0558d567156ef07cb989238b8bd56ea455d954a7
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-19 10:22:24 +0530

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,34 +4,58 @@ import {
   Grid,
   IconButton,
   InputBase,
+  makeStyles,
   Toolbar,
 } from "@material-ui/core";
 import React from "react";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#fff",
+  },
+  searchInput: {
+    opacity: "0.6",
+    padding: "0 8px",
+    fontSize: "0.8rem",
+    "&.hover": {
+      backgroundColor: "#f2f2f2",
+    },
+    "& .MuiToolbar-root": {
+      marginRight: "8px",
+    },
+  },
+});
+
 function Header() {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.root}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems="center">
           <Grid item>
-            <InputBase />
+            <InputBase
+              placeholder="Search topics "
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
           </Grid>
           <Grid item sm></Grid>
           <Grid item>
             <IconButton>
               <Badge badgeContent={4} color="secondary">
-                <NotificationsNoneIcon />
+                <NotificationsNoneIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={3} color="primary">
-                <ChatBubbleOutlineIcon />
+                <ChatBubbleOutlineIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
-              <PowerSettingsNewIcon />
+              <PowerSettingsNewIcon fontSize="small" />
             </IconButton>
           </Grid>
         </Grid>

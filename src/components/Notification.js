ff35b003abb699273f871bf14043433c79ed3905
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-23 11:26:36 +0530

    notification styles added

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,19 @@
-import { Snackbar } from "@material-ui/core";
+import { makeStyles, Snackbar } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    top: theme.spacing(9),
+  },
+}));
+
 export default function Notification(props) {
   const { notify, setNotify } = props;
+  const classes = useStyles();
   return (
     <Snackbar
+      className={classes.root}
       open={notify.isOpen}
       autoHideDuration={5000}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}

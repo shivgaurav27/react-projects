eba84bcb9ad20defc9776800bf59e99ca56cb2c4
shivgaurav7727 <shivgaurav27@gmail.com>
2021-03-23 11:30:33 +0530

    notification close

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -11,14 +11,25 @@ const useStyles = makeStyles((theme) => ({
 export default function Notification(props) {
   const { notify, setNotify } = props;
   const classes = useStyles();
+
+  const handleClose = (event, reason) => {
+    setNotify({
+      ...notify,
+      isOpen: false,
+    });
+  };
+
   return (
     <Snackbar
       className={classes.root}
       open={notify.isOpen}
       autoHideDuration={5000}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      onClose={handleClose}
     >
-      <Alert severity={notify.type}>{notify.message}</Alert>
+      <Alert severity={notify.type} onClose={handleClose}>
+        {notify.message}
+      </Alert>
     </Snackbar>
   );
 }
